4b7455ea12d78b754caae80cc3ffd9b71d285103
Ibukiyse05 <103973942+Ibukiyse05@users.noreply.github.com>
2023-02-12 16:08:19 +0900

    Update ex6-1.js

--- ex6-1.js
+++ ex6-1.js
@@ -1,2 +1,10 @@
-function sayhello() {alert('Hello, Function!');}
-function sayGoodbye() {alert('Goodbye!');}
+function sayhello68() {
+    alert('Hello, ' + document.getElementById('who68').innerHTML+ 
+    '!');
+    }
+    function taro68() {
+    document.getElementById('who68').innerHTML= '太郎';
+    }
+    function hanako68() {
+    document.getElementById('who68').innerHTML= '花子';
+    }
